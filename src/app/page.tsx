b06331a760a9391cb57dda9bed5e173e368e3197
RicoCelsius <rico.aarntzen@gmail.com>
2023-09-20 13:47:35 +0200

    added lorem ipsum

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,17 @@ export default function Home() {
   return (
     <>
       <body>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-          fermentum in quam id ullamcorper. Sed consectetur felis sed lorem
-          condimentum congue. Pellentesque habitant morbi tristique senectus et
-          netus et malesuada fames ac turpis egestas. Donec at purus
-          condimentum, gravida lacus quis, aliquam ante. Praesent imperdiet
-          justo non leo consectetur feugiat. Nam eget molestie sem, non
-          condimentum arcu. Mauris vulputate quis mauris nec ornare.
-        </p>
+        <div className="border border-gray-300 p-4 rounded-lg">
+          <p className="font-bold">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+            fermentum in quam id ullamcorper. Sed consectetur felis sed lorem
+            condimentum congue. Pellentesque habitant morbi tristique senectus
+            et netus et malesuada fames ac turpis egestas. Donec at purus
+            condimentum, gravida lacus quis, aliquam ante. Praesent imperdiet
+            justo non leo consectetur feugiat. Nam eget molestie sem, non
+            condimentum arcu. Mauris vulputate quis mauris nec ornare.
+          </p>
+        </div>
       </body>
     </>
   );
