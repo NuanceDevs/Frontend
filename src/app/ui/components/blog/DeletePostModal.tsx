60e94ded8d3965242715e690aa58b2af8f6e172d
Martijn-19 <495061@student.fonty.nl>
2023-11-03 11:19:25 +0100

    File structure working after restructuring

--- src/app/ui/components/blog/DeletePostModal.tsx
+++ src/app/ui/components/blog/DeletePostModal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "./Button";
-import { ApiGateway } from "@/app/misc/ApiGateway";
+import { ApiGateway } from "@/app/lib/ApiGateway";
 
 type DeletePostModalProps = {
   id?: number;
