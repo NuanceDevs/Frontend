9bbded57a3d2f073043fd16ae8c5346a9f70ecf2
Rico Aarntzen <rico.aarntzen@gmail.com>
2023-11-03 11:26:54 +0100

    removed tostring

--- src/app/components/blog/Card.tsx
+++ src/app/components/blog/Card.tsx
@@ -22,9 +22,7 @@ export const Card = ({
       style={{ minHeight: "250px" }}
     >
       {/* <CardImage imagePath={imagePath}></CardImage> */}
-      <a className="gp-1 text-sm">
-        Published on: {publishedOn.toString().split("T")[0]}
-      </a>
+      <a className="gp-1 text-sm">Published on:</a>
 
       <Title>{title}</Title>
 
